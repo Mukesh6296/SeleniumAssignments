0d580182522ee6bdf96cc26c3df2b6f21c9ea6b0
Mukesh6296 <159648011+Mukesh6296@users.noreply.github.com>
2024-02-14 23:48:59 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class Fibonacci {
+
+		public static void main(String [] args) {
+					
+			// Write your code here		
+
+	      int i,n=10;
+	        System.out.println("Fibonacii Series of"+n+"is");
+	      
+	      for( i=0;i<n;i++)
+	      {
+	        i+=1;
+	        System.out.println(i);
+	      }
+	     
+	      
+	      
+	      
+		}
+	}
+
+
---
+++ Pattern.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Pattern {
+
+	public static void main(String[] args) {
+	
+		int l = 6;
+		
+		for (int i=0 ; i<=l;i++) {
+			for( int j=0;j<=i;j++)
+			{
+				System.out.println("*");
+			}
+			
+			System.out.println("\n");
+		}
+		
+	
+	}
+	
+}
